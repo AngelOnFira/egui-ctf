26c7f17724c14eee1a69208347ce98d1fed487e6
Forest Anderson <forestkzanderson@gmail.com>
2023-06-28 15:29:50 -0400

    Fixed join team

--- backend/src/ctf_server/mod.rs
+++ backend/src/ctf_server/mod.rs
@@ -276,7 +276,7 @@ impl Handler<IncomingCTFRequest> for CTFServer {
                                         // coming online
                                         tasks.push(ActorTask::SendNetworkMessage(
                                             SendNetworkMessage {
-                                                to: ActorTaskTo::Team(Vec::new()),
+                                                to: ActorTaskTo::Session(msg.id),
                                                 message: NetworkMessage::CTFMessage(
                                                     CTFMessage::CTFClientStateComponent(
                                                         CTFClientStateComponent::TeamData(
@@ -374,7 +374,127 @@ impl Handler<IncomingCTFRequest> for CTFServer {
                         }
                         CTFMessage::ClientUpdate(_) => todo!(),
                         CTFMessage::Login(_) => todo!(),
-                        CTFMessage::JoinTeam(_) => todo!(),
+                        CTFMessage::JoinTeam(token) => {
+                            // Make sure the token isn't empty
+                            if token.is_empty() {
+                                CTFServer::send_message_associated(
+                                    NetworkMessage::CTFMessage(CTFMessage::ClientUpdate(
+                                        ClientUpdate::Notification(
+                                            "Token cannot be empty".to_string(),
+                                        ),
+                                    )),
+                                    recipient_clone,
+                                );
+
+                                // Return tasks
+                                return tasks;
+                            }
+
+                            // See if there is a team with this token
+                            let team: Option<team::Model> = team::Entity::find()
+                                .filter(team::Column::JoinToken.eq(&token))
+                                .one(&db_clone)
+                                .await
+                                .expect("Failed to check if team exists");
+
+                            match team {
+                                // If no team exists with this token, return an
+                                // error message
+                                None => {
+                                    CTFServer::send_message_associated(
+                                        NetworkMessage::CTFMessage(CTFMessage::ClientUpdate(
+                                            ClientUpdate::Notification(
+                                                "No team exists with this token".to_string(),
+                                            ),
+                                        )),
+                                        recipient_clone,
+                                    );
+
+                                    // Return tasks
+                                    return tasks;
+                                }
+                                Some(team) => {
+                                    // Get the hacker associated with this
+                                    // request
+                                    let hacker: hacker::Model = hacker::Entity::find()
+                                        .filter(hacker::Column::DiscordId.eq(&discord_id))
+                                        .one(&db_clone)
+                                        .await
+                                        .expect("Failed to get hacker")
+                                        .unwrap();
+
+                                    // If this hacker is already on a team,
+                                    // return an error message
+                                    if hacker.fk_team_id.is_some() {
+                                        CTFServer::send_message_associated(
+                                            NetworkMessage::CTFMessage(CTFMessage::ClientUpdate(
+                                                ClientUpdate::Notification(
+                                                    "You are already on a team".to_string(),
+                                                ),
+                                            )),
+                                            recipient_clone,
+                                        );
+
+                                        // Return tasks
+                                        return tasks;
+                                    }
+
+                                    // Update the hacker's team id
+                                    let mut hacker: hacker::ActiveModel = hacker.into();
+                                    hacker.fk_team_id = Set(Some(team.id));
+                                    let hacker_id = hacker.clone().discord_id.unwrap();
+                                    hacker
+                                        .save(&db_clone)
+                                        .await
+                                        .expect("Failed to update hacker");
+
+                                    // Send the hacker a message that they
+                                    // joined a team
+                                    tasks.push(ActorTask::SendNetworkMessage(SendNetworkMessage {
+                                        to: ActorTaskTo::Session(msg.id),
+                                        message: NetworkMessage::CTFMessage(
+                                            CTFMessage::CTFClientStateComponent(
+                                                CTFClientStateComponent::ClientData(
+                                                    CTFState::get_hacker_client_data(
+                                                        &hacker_id, &db_clone,
+                                                    )
+                                                    .await,
+                                                ),
+                                            ),
+                                        ),
+                                    }));
+
+                                    // Send the hacker their team data
+                                    tasks.push(ActorTask::SendNetworkMessage(
+                                        SendNetworkMessage {
+                                            to: ActorTaskTo::Session(msg.id),
+                                            message: NetworkMessage::CTFMessage(
+                                                CTFMessage::CTFClientStateComponent(
+                                                    CTFClientStateComponent::TeamData(
+                                                        CTFState::get_hacker_team_data(
+                                                            &hacker_id, &db_clone,
+                                                        )
+                                                        .await,
+                                                    ),
+                                                ),
+                                            ),
+                                        },
+                                    ));
+
+                                    // Send the hacker a notification that they
+                                    // joined a team
+                                    CTFServer::send_message_associated(
+                                        NetworkMessage::CTFMessage(CTFMessage::ClientUpdate(
+                                            ClientUpdate::Notification(format!(
+                                                "You joined team {}",
+                                                team.name
+                                            )),
+                                        )),
+                                        recipient_clone,
+                                    );
+                                }
+                            }
+                        }
                         CTFMessage::CreateTeam(team_name) => {
                             // TODO: Check if this user is already on a team
 
