d785fe3814bfe10c2186aa5911cb5aa962c60bd7
Forest Anderson <forestkzanderson@gmail.com>
2023-06-29 20:48:13 -0400

    Working on challenge list

--- frontend/src/panels/challenge_list.rs
+++ frontend/src/panels/challenge_list.rs
@@ -1,6 +1,8 @@
-use common::ctf_message::{CTFState, GameData};
+use common::ctf_message::{CTFChallenge, CTFState, GameData};
 use eframe::egui;
+use egui::{epaint::ahash::HashMap, ScrollArea};
 use egui_extras::{Column, TableBuilder};
+use itertools::Itertools;
 
 use crate::app::ClientState;
 
@@ -34,11 +36,37 @@ impl ChallengePanel {
     }
 
     fn ui(&mut self, ui: &mut egui::Ui, ctf_state: &ClientState) {
-        ui.add_enabled_ui(self.enabled, |ui| {
+        ScrollArea::vertical().max_height(400.0).show(ui, |ui| {
             // If we're logged in, show all the challenges
             if let GameData::LoggedIn { challenges } = &ctf_state.ctf_state.game_data {
-                for challenge in challenges {
-                    ui.label(&challenge.title);
+                // Sort the challenges into categories
+                let sorted_challenges: HashMap<String, Vec<CTFChallenge>> =
+                    challenges
+                        .iter()
+                        .fold(HashMap::default(), |mut acc, challenge| {
+                            acc.entry(challenge.category.clone())
+                                .or_insert_with(Vec::new)
+                                .push(challenge.clone());
+                            acc
+                        });
+
+                // Show the challenges. Show the categories sorted by name. Use
+                // a header for each category name, then show the challenges in
+                // that category using a label. Sort the challenges by point
+                // value.
+                for (category, challenges) in
+                    sorted_challenges.iter().sorted_by(|a, b| a.0.cmp(&b.0))
+                {
+                    ui.heading(category);
+                    ui.separator();
+                    let mut challenges = challenges.clone();
+                    challenges.sort_by(|a, b| a.points.cmp(&b.points));
+                    for challenge in challenges {
+                        if ui
+                            .button(format!("{} ({} points)", challenge.title, challenge.points))
+                            .clicked()
+                        {}
+                    }
                 }
             }
         });
