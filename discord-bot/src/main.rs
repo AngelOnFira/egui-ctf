cbcc0b3b621c3799b921a10b250ddca8a502d713
Forest Anderson <forestkzanderson@gmail.com>
2023-07-04 17:18:26 -0400

    Basic shuttle.rs setup

--- discord-bot/src/main.rs
+++ discord-bot/src/main.rs
@@ -9,6 +9,7 @@ use serenity::model::application::interaction::{Interaction, InteractionResponse
 use serenity::model::gateway::Ready;
 use serenity::model::id::GuildId;
 use serenity::prelude::*;
+use shuttle_secrets::SecretStore;
 
 struct Handler {
     db: DatabaseConnection,
@@ -89,3 +90,29 @@ async fn main() {
         println!("Client error: {:?}", why);
     }
 }
+
+// #[shuttle_runtime::main]
+// async fn serenity(
+//     #[shuttle_secrets::Secrets] secret_store: SecretStore,
+//     #[shuttle_shared_db::Postgres(
+//         local_uri = "postgres://postgres:{secrets.PASSWORD}@localhost:16695/postgres"
+//     )]
+//     pool: PgPool,
+// ) -> shuttle_serenity::ShuttleSerenity {
+//     // Get the discord token set in `Secrets.toml`
+//     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+
+//     let db = Database::connect("postgres://postgres:password@localhost:5432/shuttle")
+//         .await
+//         .unwrap();
+
+//     // Set gateway intents, which decides what events the bot will be notified about
+//     let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+
+//     let client = Client::builder(&token, intents)
+//         .event_handler(Bot)
+//         .await
+//         .expect("Err creating client");
+
+//     Ok(client.into())
+// }
